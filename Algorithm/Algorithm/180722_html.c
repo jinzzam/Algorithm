39b772475070fbe5fb1b0058889384247e1a88cd
jinzzam <firstmind_@naver.com>
2018-07-22 23:00:31 +0900

    add IO system

---
+++ Algorithm/Algorithm/180722_html.c
@@ -0,0 +1,20 @@
+//
+//  180722_html.c
+//  Algorithm
+//
+//  Created by jinzzam on 2018. 7. 22..
+//  Copyright © 2018년 jinzzam. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char html[] = {};
+    
+    printf("html 코드를 입력하세요 : ");
+    while(scanf("%s", html)!= EOF)
+        printf("%s ", html);
+    
+    return 0;
+}
