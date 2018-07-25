cdde4103676b859ff1fdd2a90ee620cf5df44dc6
jinzzam <firstmind_@naver.com>
2018-07-25 22:56:13 +0900

    add main

--- Java/src/com/jinzzam/alg/Main.java
+++ Java/src/com/jinzzam/alg/Main.java
@@ -1,8 +1,23 @@
 package com.jinzzam.alg;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static int wordCount;
+    public static StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        String line;
+        while ((line = in.readLine()) != null) toHTML(line);
+        out.write(stringBuilder.toString());
+        out.close();
+        in.close();
+    }
+
+    private static void toHTML(String line) {
+        //do something
     }
 }
